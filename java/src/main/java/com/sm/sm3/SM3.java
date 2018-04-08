1b04c38cc342accdc1ce66aa4b1175410873d5de
William Sun <william.sun@pku.edu.cn>
2018-04-08 12:28:08 -0400

    Update SM3.java

--- java/src/main/java/com/sm/sm3/SM3.java
+++ java/src/main/java/com/sm/sm3/SM3.java
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2016 BeiJing JZYT Technology Co. Ltd
- * www.idsmanager.com
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * BeiJing JZYT Technology Co. Ltd ("Confidential Information").
- * You shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement you
- * entered into with BeiJing JZYT Technology Co. Ltd.
- */
 package com.sm.sm3;
 
 import java.io.ByteArrayOutputStream;
